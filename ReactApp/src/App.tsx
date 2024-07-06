07c8ef172872c2210c20a1cb26e91b614b8d98e9
pria-pria <thepriyaduggal@gmail.com>
2024-07-06 08:29:08 +0530

    first commit

---
+++ ReactApp/src/App.tsx
@@ -0,0 +1,8 @@
+
+import Message from "./Message";
+
+function App(){
+  return <div><Message></Message></div>;
+}
+
+export default App;
---
+++ ReactApp/src/Message.tsx
@@ -0,0 +1,8 @@
+
+
+function Message() {
+
+    return <h1>Hello World;</h1>;
+}
+
+export default Message;
